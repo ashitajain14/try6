2026169f710d58f81896bd4cff7b19a04e97c469
Aashita Jain <aashitajain@Aashitas-MacBook-Air.local>
2020-06-07 20:43:22 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'try6';
+export class AppComponent implements OnInit {
+    title = 'try6';
+
+    columnDefs = [
+        {headerName: 'Make', field: 'make', filter: true},
+        {headerName: 'Price', field: 'price', sortable: true},
+        {headerName: 'Make2', field: 'make', filter: true},
+        {headerName: 'Model', field: 'model', filter: true},
+    ];
+
+    rowData: any;
+
+    constructor(private http: HttpClient) {
+
+    }
+
+    ngOnInit() {
+        this.rowData = this.http.get('https://raw.githubusercontent.com/ashitajain14/Data/master/test2.json');
+    }
 }
